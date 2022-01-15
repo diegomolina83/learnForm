4b96d6cb26743dc269f3bd82430185828a79b55b
[Diego Molina] <[diegomolina83@gmail.com]>
2022-01-15 18:44:52 +0100

    Learning Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface Task {
+  name: string,
+  done: boolean
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learnForm';
+today: Date= new Date();
+tasks: Task[]=[];
+tareaRealizada:boolean = false;
+indexOfElement!: number;
+
+
+form= new FormGroup({
+  tarea: new FormControl(''),
+})
+
+
+
+putInList(){
+
+this.tasks.push({name: this.form.value.tarea, done:false})
+this.form.reset();
 }
+
+cross(index:number){
+  this.tasks[index].done = !this.tasks[index].done
+
+}
+
+delete(index:number){
+  console.log(this.tasks.length);
+  console.log(this.tasks)
+  this.tasks.splice(index, 1)
+  console.log(this.tasks)
+
+}
+}
+
